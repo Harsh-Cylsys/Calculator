c18ef702e0aec194d4a80484b87dcb71a952af3a
Harsh-Cylsys <Harsh.Goyal@cylsys.com>
2024-07-13 11:48:43 +0530

    Update 11:48 script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const input = document.getElementById("user-input");
+let flag = false;
 
 const number = document.querySelectorAll(".numbers").forEach(function (item) {
     item.addEventListener("click", function (e) {
+      if(flag){
+        input.innerText="";
+        flag=false;
+      }
       if (input.innerText === "NaN") {
         input.innerText = "";
       }
@@ -12,30 +17,32 @@ const number = document.querySelectorAll(".numbers").forEach(function (item) {
     });
   });
 
-  const calculate = document
-  .querySelectorAll(".operations")
-  .forEach(function (item) {
-    item.addEventListener("click", function (e) {
-      console.log(e.target.innerHTML);
-      
-      let lastValue = input.innerText.substring(input.innerText.length, input.innerText.length-1);
-      if (!isNaN(lastValue) && e.target.innerHTML === "=") {
-          input.innerText = eval(input.innerText);
-      } 
-      else if (e.target.innerHTML === "AC") {
-          input.innerText = 0;
-      } 
-      else if (e.target.innerHTML === "DEL") {
-          input.innerText = input.innerText.substring(0,input.innerText.length - 1);
-          if (input.innerText.length == 0) {
-              input.innerText = 0;
-          }
-      } 
-      else {
-          if (!isNaN(lastValue)){
-              input.innerText += e.target.innerHTML;
-          }
-      }
-      
+  const calculate = document.querySelectorAll(".operations")
+    .forEach(function (item) {
+      item.addEventListener("click", function (e) {
+        console.log(e.target.innerHTML);
+        
+        let lastValue = input.innerText.substring(input.innerText.length, input.innerText.length-1);
+        if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+            input.innerText = eval(input.innerText);
+            flag=true;
+        } 
+        else if (e.target.innerHTML === "AC") {
+            input.innerText = 0;
+            flag=false;
+        } 
+        else if (e.target.innerHTML === "DEL") {
+            input.innerText = input.innerText.substring(0,input.innerText.length - 1);
+            if (input.innerText.length == 0) {
+                input.innerText = 0;
+            }
+            flag=false;
+        } 
+        else {
+            if (!isNaN(lastValue)){
+                input.innerText += e.target.innerHTML;
+            }
+            flag=false;
+        }
     });
   });
